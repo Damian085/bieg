c5e7990f3127a622ff0d9542145703e4e46d3c12
Damian085 <72349431+Damian085@users.noreply.github.com>
2022-01-01 18:58:07 +0100

    Update mail.php

--- admin/mail/mail.php
+++ admin/mail/mail.php
@@ -1,37 +1,33 @@
-<?php 
+<?php
 
-    function mailAll($mail,$imie,$nazwisko){
-                    $server = 'localhost';
-                    $user = 'u235805072_bembol';
-                    $pwd = 'B3mbol123!';
-                    $name = 'u235805072_bieg';
-                    
-                    $conn = mysqli_connect("$server","$user","$pwd","$name");
-                    $query = mysqli_query($conn,"SELECT * FROM `zawodnicy` WHERE accept = 1");
-                    
-                    while($row = mysqli_fetch_row($query)){
-                        $to = $row[0];
-                    }
-                    $subject = "Potwierdzenie zg�oszenia";
-                    $message = "Witaj $imie $nazwisko. 
-                            To jest twoja rezerwacja 
-                            A to tw�j mail $mail";
-                    $headers = 'From: admin@biegniemybypomagac.pl' . "\r\n" .
-                    'Reply-To: admin@biegniemybypomagac.pl' . "\r\n" .
-                    'X-Mailer: PHP/' . phpversion();
-                        
-                        mail($to, $subject, $message, $headers); 
-                    
-                    }
+
+
+    function mailAll(){
+        $server = 'localhost';
+        $user = 'u235805072_bembol';
+        $pwd = 'B3mbol123!';
+        $name = 'u235805072_bieg';
+        
+        $conn = mysqli_connect("$server","$user","$pwd","$name");
+        $query = mysqli_query($conn,"SELECT email FROM `zawodnicy` WHERE accept = 1");
+
+        $mail = null;
+        while($row = mysqli_fetch_array($query, MYSQLI_BOTH)){
+            if (!$mail){ 
+                $mail = $row['email'];
+            }else{
+                $mail = $mail . ", " . $row['email'];
+            }
+        }
+
+        $subject = $_POST['title'];
+        $message = $_POST['tresc'];
+        $headers = 'From: admin@biegniemybypomagac.pl' . "\r\n" .
+        'Reply-To: admin@biegniemybypomagac.pl' . "\r\n" .
+        'X-Mailer: PHP/' . phpversion();
+            
+            mail($mail, $subject, $message, $headers); 
+        
+    }
                    
-                    $server = 'localhost';
-                    $user = 'u235805072_bembol';
-                    $pwd = 'B3mbol123!';
-                    $name = 'u235805072_bieg';
                     
-                    $conn = mysqli_connect("$server","$user","$pwd","$name");
-                    $query = mysqli_query($conn,"SELECT email FROM `zawodnicy`");
-                    
-                    while($row = mysqli_fetch_row($query)){
-                        $to = $row[0];
-                    }
